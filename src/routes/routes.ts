fe1dd0478e79f84770be91ba66e537f9274fd58c
Lucas-Bento-D <lucasbentoskl@outlook.com>
2024-11-04 22:32:38 -0300

    Feat: add recoveryPassEmail

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -3,6 +3,7 @@ import checkToken from "../core/checkToken";
 import { Request, Response } from "express"
 import UsersController from "../controllers/users.controller";
 import { transporter } from "../email/config/config";
+import generateCode from "../utils/generateCode";
 
 const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
@@ -24,6 +25,26 @@ router.get("/user/:id", checkToken, users.Get)
 router.post("/auth/forgetPassword", users.ForgetPassword)
 
 router.delete("/user/deleteAccount/", checkToken, users.Delete)
+
+
+router.patch("/recoveryPassEmail", users.UpdateOTP, async (req: any, res: any) => {
+    const {email} = req.body
+    const mailOptions = {
+        from: "noreply.bentodesenvolvimentos@gmail.com",
+        to: email,
+        subject: "teste de envio de código",
+        text: `code: ${req.recoveryPass.code} \n data de expiração: ${req.recoveryPass.expirationDate}`
+    }
+    
+    try{
+
+        await transporter(process.env.APP_EMAIL, process.env.APP_EMAIL_PASS).sendMail(mailOptions)
+        return res.status(200).json({message: "email enviado"})
+    }catch(error){
+        console.log(error)
+    }
+})
+
 
 router.patch("/user/update", checkToken,  users.Update)
 
